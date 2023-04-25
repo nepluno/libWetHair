dfa761d6528443727b8d5af45fc456ee0442addd
Fredrik Salomonsson <plattfot@posteo.net>
2023-04-25 11:51:30 -0700

    Add delegateLogOutput function
    Which will just try and open the path for writing, if it succeeds it
    will pass on the std::ostream to the write. If not it will just use
    stream. This to make sure that the info in the write function gets
    written to somewhere.

--- libWetHair/LogUtilities.h
+++ libWetHair/LogUtilities.h
@@ -11,6 +11,9 @@
 #define LIBWETHAIR_CORE_LOG_UTILITIES_H_
 
 #include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <ostream>
 #include <string_view>
 
 namespace libwethair {
@@ -20,6 +23,29 @@ namespace libwethair {
 /// The path will be <temp directory>/<prefix><timestamp>.log
 std::filesystem::path tempLogFile(const std::string_view prefix,
                                   const char* timestamp_format = "%Y%m%dT%H%M%S");
+
+/// Call write with either an std::ostream of path or stream.
+///
+/// Where the interface for write is:
+///
+///   void write(std::ostream&)
+///
+/// It will try to open the file pointed to by path and pass that to
+/// write. If it succeeds it will write a message to stream containing
+/// the path name. If it fails to open path it will pass stream to
+/// write instead.
+template<typename Writer>
+void delegateLogOutput(const std::filesystem::path& path,
+                       const Writer& write,
+                       std::ostream& stream = std::cerr)
+{
+  if (std::ofstream log(path); log.is_open()) {
+    write(log);
+    stream << "Saved log to: " << path <<"\n";
+  } else {
+    write(stream);
+  }
+}
 
 }  // namespace libwethair
 
